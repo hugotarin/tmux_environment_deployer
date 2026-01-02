a7adda3b649cfe0397d74431ed039e503b859cba
Hugo <hugo.tarin.empren@gmail.com>
2026-01-02 13:38:31 +0100

    Mod tmux_envi_dep.sh

--- tmux_envi_dep.sh
+++ tmux_envi_dep.sh
@@ -13,6 +13,8 @@ tmux renamew -t "$SESSION:0" "$WINDOW1"
 tmux splitw -v -l 10
 tmux splitw -h -l 100
 
+tmux list-windows
+
 tmux neww -S -t "$SESSION" -n "$WINDOW2" -c "$DIR"
 
 zathura $DIR/pdf/$NAME.pdf &
