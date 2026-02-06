1daa65770cea1f6665506d471ffc12ac10f574cc
Hugo Tarin <hugo.tarin.empren@gmail.com>
2026-02-06 16:29:56 +0100

    Add -p option functionality

--- tmux_envi_dep.sh
+++ tmux_envi_dep.sh
@@ -2,7 +2,7 @@
 cd $1
 
 DIR=$1
-PDFDIR="$DIR/pdf/"
+PDFDIR="~/${DIR}pdf/"
 SESSION="Typst Envi"
 WINDOW1="Editor"
 WINDOW2="Visor"
@@ -19,15 +19,20 @@ tmux renamew -t "$SESSION:0" "$WINDOW1"
 tmux send-keys -t "$SESSION:$WINDOW1.0" "cd $PDFDIR" C-m
 tmux send-keys -t "$SESSION:$WINDOW1.0" "nvim $NAME.typ" C-m
 
-# Crear el paneles 1 y 2
+# Crear el panel 1
 tmux splitw -t "$SESSION:$WINDOW1.0" -v -l 1
-tmux splitw -t "$SESSION:$WINDOW1.0" -h 
 
 # Ejecutar comando 'watch'
-tmux send-keys -t "$SESSION:$WINDOW1.2" "typst watch $PDFDIR$NAME.typ" C-m
+tmux send-keys -t "$SESSION:$WINDOW1.1" "typst watch $PDFDIR$NAME.typ" C-m
 
-# Visualizar 'plantilla.typ'
-tmux send-keys -t "$SESSION:$WINDOW1.1" "nvim $(PDFDIR)plantilla.typ" C-m
+if [ $# -eq 2 ]; then
+	if [ "$2" = "-p"  ]; then
+		# Visualizar 'plantilla.typ'
+		tmux splitw -t "$SESSION:$WINDOW1.0" -h 
+		tmux send-keys -t "$SESSION:$WINDOW1.1" "cd $PDFDIR" C-m
+		tmux send-keys -t "$SESSION:$WINDOW1.1" "nvim plantilla.typ" C-m
+	fi
+fi
 
 # Crear la ventana 1 'Visor'
 tmux neww -S -t "$SESSION" -n "$WINDOW2" -c "$DIR"
@@ -35,7 +40,7 @@ tmux neww -S -t "$SESSION" -n "$WINDOW2" -c "$DIR"
 # Visualizar el PDF con 'zathura'
 tmux send-keys -t "$SESSION:$WINDOW2" "zathura $PDFDIR$NAME.pdf &" C-m C-l
 
-# Volver a la ventana 0
+# Volver a la ventana 1
 tmux selectw -t "$SESSION:$WINDOW1"
 
 # Conectarse a la sesión 'Typst Envi'
